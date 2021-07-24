9aa58561deedc1361df470fb76c84e03d272bbea
vivette-m <86829922+vivette-m@users.noreply.github.com>
2021-07-24 17:31:56 +0100

    Add files via upload

---
+++ #TASK 2-markgrade.py
@@ -0,0 +1,26 @@
+#TASK 2
+
+
+range (0,100) 
+grade = float(input("what is the grade percentage")
+print ("percentage_mark" "100%") + ("mark_grade")
+if grade >=90:
+print("you got A")
+elif 80<=grade<90:
+print("you got B")
+elif 70<=grade<80:
+print("you got C")
+elif 60<=grade<70:
+print("you got D")
+else:
+    print("sorry you need grade D to pass")
+return final-grade
+
+
+
+
+  
+
+
+
+
