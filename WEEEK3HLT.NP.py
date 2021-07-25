374a8e4660344f2718ec2f4c7f3d5d058df3dee4
vivette-m <86829922+vivette-m@users.noreply.github.com>
2021-07-25 15:56:29 +0100

    Add files via upload

---
+++ WEEEK3HLT.NP.py
@@ -0,0 +1,47 @@
+
+import numpy as np
+a = np.array([0,1,2,3,4,5,6,7,8,9])
+print(a)
+
+
+import numpy as np
+bool_arr = np.ones (3,3),np.dtype=bool
+print(bool_arr)
+
+import numpy as np
+a = np.array ([1,2,3,4,5,6,7,8,9,10])
+filter_a = array % 2 == 1
+newarr = array [filter_arr]
+print(filter_array)
+print(newarr)
+
+import numpy as np
+arr = np.arange(1,10)
+arr
+a = np.array([1,2,3,4,5,6,7,8,9,10])
+array[1,3,5,7,,9]
+arr = -1
+
+
+import numpy as np
+
+a = np.array ([1,2,3,4,5,6,7,8,9,10])
+new_arr = array.reshape(2X5)
+print(new_array)
+
+import numpy as np
+a = np array(a)([1,2,3,4,])
+a = np array(b)([5,6,7,8,])
+print(a,b)
+array = np.dot (a)
+print(a)
+array = np.sum(b)
+print(b)
+
+
+
+
+
+
+
+
